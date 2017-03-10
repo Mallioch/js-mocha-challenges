38c7c72a9bd5ec73fb1e9a953b7ae62d44a66439
Eric Sowell <eric.sowell@gmail.com>
2017-03-09 21:56:49 -0600

    improvements to conditionals

--- section-1/intro-to-conditionals/intro-to-conditionals.js
+++ section-1/intro-to-conditionals/intro-to-conditionals.js
@@ -10,3 +10,34 @@ function fullName(firstName, lastName, middleInitial) {
 
   return firstName + ' ' + middleInitial + ' ' + lastName;
 }
+
+
+
+function returnSmaller(first, second) {
+  if (first < second) {
+    return first;
+  }
+  else if (second < first) {
+    return second;
+  }
+  else {
+    return first;
+  }
+}
+
+
+
+function tankDeterminer(animal, size) {
+  if (animal === 'turtle') {
+    return 'turtle';
+  }
+  else if (animal === 'clown fish' && size === 'small') {
+    return 'small saltwater';
+  }
+  else if (animal === 'clown fish' && size === 'large') {
+    return 'medium saltwater';
+  }
+  else if (animal === 'guppy') {
+    return 'small freshwater';
+  }
+}
